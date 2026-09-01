b82020276a47855529c769bd1d862ab782bd894d
dinnarafizaaz <dinnarafizaaz310@gmail.com>
2026-09-01 08:57:26 +0700

    adding dragging widget

---
+++ lib/widgets/sheet_drag_handle.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+//GARIS KECIL ABU/SHEET UI SEBAGAI TANDA BAHWA SCREEN/SHEET TERSEBUT BISA DI DRAG KE ATAS
+class SheetDragHandle extends StatelessWidget {
+  const SheetDragHandle({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        width: 44,
+        height: 5,
+        decoration: BoxDecoration(
+          color: Colors.grey.shade200,
+          borderRadius: BorderRadius.circular(3)
+        ),
+      ),
+    );
+  }
+}
